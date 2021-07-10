3a9387c32dba5bc6164f363e992b4e0f5281d048
Гладышев Дмитрий <nightflash@live.ru>
2021-07-11 01:36:43 +0700

    Ожидание ответа операции чтения UID и возврат данных

--- rfidCard.py
+++ rfidCard.py
@@ -3,12 +3,19 @@ from hidDevice import hidDevice
 class rfidCard():
     def __init__(self):
         self.hid = hidDevice(vid = 0x1EAF, pid = 0x0030, callback = self.callback)
+        self.waitdata = False
 
     def __del__(self):
         del(self.hid)
 
     def callback(self, rawdata):
+        self.rawdata = rawdata
+        self.waitdata = False
         print(">> ", rawdata)
 
     def readUID(self):
+        self.waitdata = True
         self.hid.writeHID([0x01])
+        while self.waitdata:
+            pass
+        return self.rawdata
